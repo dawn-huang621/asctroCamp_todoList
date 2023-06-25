c3a11ba35c33a4ec6780f7a78cac8eaabd89e118
q2379685 <q2379685@gmail.com>
2023-06-25 22:50:32 +0800

    修改checkbox的資料比對與渲染，新增編輯功能

--- index.js
+++ index.js
@@ -31,18 +31,29 @@ function showList (data) {
     if(data){
         let str = ''
         data.forEach((item , index)=> {
-            str += 
-            `<li class="${index}">
-                <input type="checkbox" class="is-checked">
-                ${item.text}
-                <div class="btn-all">
-                    <button type="button" class="edit-btn btn btn-primary">編輯</button>
-                    <button type="button" class="del-btn btn btn-primary">刪除</button>
-                </div>
-            </li>`
+            if(item.finished){
+                str += 
+                `<li class="${index}" id="finished-line">
+                    <input type="checkbox" class="is-checked" checked>
+                    ${item.text}
+                    <div class="btn-all">
+                        <button type="button" class="edit-btn btn btn-primary">編輯</button>
+                        <button type="button" class="del-btn btn btn-primary">刪除</button>
+                    </div>
+                </li>`
+            }else{
+                str += 
+                `<li class="${index}">
+                    <input type="checkbox" class="is-checked">
+                    ${item.text}
+                    <div class="btn-all">
+                        <button type="button" class="edit-btn btn btn-primary">編輯</button>
+                        <button type="button" class="del-btn btn btn-primary">刪除</button>
+                    </div>
+                </li>`
+            }
         });
         addInput.value = ''
-        // console.log(str)
         ul.insertAdjacentHTML("beforeend", str) 
     }
 }
@@ -53,29 +64,57 @@ ul.addEventListener("click",(e)=>{
     // console.log(e.target.parentNode)
     // console.log(e.target.parentNode.className)
     // console.log(data)
-    data.forEach((list, index) =>{
-        let count = 0
-        // console.log(index, Number(e.target.parentNode.className))
-        id = Number(e.target.parentNode.className)
-        if(index === id){
-            list.finished = !list.finished
-            // console.log(data)
-            console.log(list.finished)
-            if(list.finished){
-                e.target.parentNode.style['text-decoration'] = 'line-through'
-            } else {
-                e.target.parentNode.style['text-decoration'] = 'none'
-            }
-        }
-        // console.log(list.finished)
     
-    // console.log(e.target.className.includes("edit-btn"))
-        if(e.target.className.includes("del-btn")){
+    if(e.target.className.includes("is-checked")){
+        data.forEach((item, index)=>{
+            id = Number(e.target.parentNode.className)
+            if(index === id){
+                item.finished = !item.finished
+                localStorage.setItem("todos", JSON.stringify(data))
+                showList (data)
+                // console.log(e.target.parentNode.checked)
+                
+                // console.log(item.finished)
+            }
+        })
+    }
+    // data.forEach((list, index) =>{
+    //     let count = 0
+    //     id = Number(e.target.parentNode.className)
+    //     if(index === id){
+    //         list.finished = !list.finished
+    //         console.log(list.finished)
+    //         if(list.finished){
+    //             e.target.parentNode.style['text-decoration'] = 'line-through'
+    //         } else {
+    //             e.target.parentNode.style['text-decoration'] = 'none'
+    //         }
+    //         localStorage.setItem("todos", JSON.stringify(data))
+    //         showList (data)
+    //     }
+    // })
+    
+    if(e.target.className.includes("del-btn")){
+        if(index === Number(e.target.parentNode.parentNode.className)){
+            data.splice(index, 1)
+            localStorage.setItem("todos", JSON.stringify(data))
+            showList (data)
+        }
+    }    
+    if(e.target.className.includes("edit-btn")){
+        let content = prompt("請輸入更改內容")
+        console.log(content)
+        // console.log(e.target.parentNode.parentNode.className)
+        data.forEach((item, index)=>{
             if(index === Number(e.target.parentNode.parentNode.className)){
-                data.splice(index, 1)
+                item.text = content
                 localStorage.setItem("todos", JSON.stringify(data))
                 showList (data)
             }
-        }
+        })
+    }
+
+    data.forEach(item =>{
+        item.finished
     })
 })
